1578b5f60728763e9169a9aac6f38d008cb18735
qingyezhu <xiaoxian1369@126.com>
2015-12-13 23:19:54 +0800

    add jquery plugin

---
+++ js/masklayer.js
@@ -0,0 +1,109 @@
+;(function($){
+	$.fn.MaskLayer = function(options){
+		var defaults = {
+			msg: '请稍候',
+			img: 'waiting.gif',
+			body: $(document.body),
+			maskLayerCssName: 'mask-layer',
+			conentLayerCssName: 'mask-layer-conent',
+			msgMaskLayerCssName: 'mask-layer-msg',
+			imgMaskLayerCssName: 'mask-layer-img',
+			autoOpen: false
+		};
+		
+		var settings = $.extend({}, defaults, options);
+
+		var methods = {
+			init: function(){
+				//初始化方法
+				var that = this;
+
+				//创建遮罩层
+				methods.createMaskLayer.call(that);
+
+				//是否打开遮罩层
+				var autoOpen = settings.autoOpen;
+				if(autoOpen){
+					//打开遮罩层
+					methods.openMask.call(that);
+
+				}
+				return that;
+			},
+			createMaskLayer: function(){
+				//创建遮罩层方法
+
+				var that = this;
+				that.prop('class', settings.maskLayerCssName);
+				
+				var $msg = $('<span></span>').prop('class', settings.msgMaskLayerCssName).html(settings.msg);
+
+				var $img = $('<img/>').prop('class', settings.imgMaskLayerCssName).prop('src', settings.img);
+
+				var $content = $('<div></div>').prop('class', settings.conentLayerCssName);
+
+				$img.appendTo($content);
+				$msg.appendTo($content);
+				that.append($content).css('display', 'none');
+
+				return that;
+			},
+			openMask: function(msg){
+				//打开遮罩层方法
+
+				var that = this;
+				$.fn.MaskLayer.showMask.call(that, msg);
+				return that;
+			},
+			closeMask: function(){
+				//关闭遮罩层方法
+
+				var that = this;
+				$.fn.MaskLayer.hideMask.call(that);
+				return that;
+			}
+
+		};
+
+		var method = arguments[0];
+
+		if(methods[method]){
+			method = methods[method];
+			arguments = Array.prototype.slice.call(arguments, 1);
+		}else if(typeof method === 'object' || !method){
+			method = methods['init'];
+		}else{
+			$.error('method ' + method + ' does not exist on jQuery.MaskLayer');
+			return this;
+		}
+
+		return method.apply(this, arguments);
+	};
+
+	$.fn.MaskLayer.showMask = function(msg){
+		//重写遮罩层显示方法
+		var that = this;
+
+		//重置遮罩提示
+		msg && that.find('span').html(msg);
+
+		var height = that.css('height');
+		that.css({'opacity': 1, 'display': 'block', 'top': '-' + height.substr(0, height.length - 2) + 'px'})
+
+		that.animate({top: 0}, 'normal');
+		return that;
+	}
+	$.fn.MaskLayer.hideMask = function(){
+		//重写遮罩层隐藏方法
+		var that = this;
+
+		var height = that.css('height');
+
+		that.animate({'opacity': 0, 'top': height.substr(0, height.length - 2) + 'px'}, 'normal', null, function(){
+			that.css('display', 'none');
+		});
+		return that;
+	}
+
+
+})(jQuery);
---
+++ js/tipMessage.js
@@ -0,0 +1,97 @@
+;(function(){
+	$.fn.tipMessage = function(options){
+		var defaults = {
+			msg: '提示信息',
+			tipMessageCssName: 'tip-message',
+			contentCssName: 'tip-message-content',
+			msgCssName: 'tip-message-msg',
+			autoOpen: false
+		};
+
+		var settings = $.extend({}, defaults, options);
+
+		var methods = {
+			init: function(){
+				//初始化方法
+
+				//创建消息提示
+				methods.createTipMessage.call(this);
+
+				//是否打开消息提示
+				if(settings.autoOpen){
+					//打开消息提示
+					methods.openTipMessage.call(this);
+				}
+				return this;
+			},
+			createTipMessage: function(){
+				//创建消息提示
+				var that = this;
+
+				that.prop('class', settings.tipMessageCssName);
+
+				var $msg = $('<span></span>').prop('class', settings.msgCssName).html(settings.msg);
+
+				var $content = $('<div></div>').prop('class', settings.contentCssName);
+
+				$msg.appendTo($content);
+				that.append($content).css('display', 'none');
+
+				return that;
+			},
+			openTipMessage: function(msg){
+				//打开消息提示
+				var that = this;
+				$.fn.tipMessage.showTip.call(that, msg);
+				return that;
+
+			},
+			closeTipMessage: function(){
+				//关闭消息提示
+				var that = this;
+				$.fn.tipMessage.hideTip.call(that);
+				return that;
+			}
+		};
+
+		var method = arguments[0];
+		if(methods[method]){
+			method = methods[method];
+			arguments = Array.prototype.slice.call(arguments, 1);
+		}else if(typeof method === 'object' || !method){
+			method = methods['init'];
+		}else{
+			$.error('method ' + method + ' does not exist on jQuery.tipMessage');
+			return this;
+		}
+		return method.apply(this, arguments);
+	}
+
+	$.fn.tipMessage.showTip = function(msg){
+		//打开消息提示
+		var that = this;
+
+		//重置消息
+		msg && that.find('span').html(msg);
+
+		var height = that.css('height');
+
+		that.css({'opacity': 1, 'display': 'block', 'top': '-' + height.substr(0, height.length - 2) + 'px'});
+
+		that.animate({top: 0}, 'normal');
+		return that;
+	}
+
+	$.fn.tipMessage.hideTip = function(){
+		//关闭消息提示
+		var that = this;
+
+		var height = that.css('height');
+
+		that.animate({'opacity': 0, 'top': '-' + height.substr(0, height.length - 2) + 'px'}, 'normal', null, function(){
+			that.css({'display': 'none'});
+		});
+		return that;
+	}
+
+})(jQuery);
